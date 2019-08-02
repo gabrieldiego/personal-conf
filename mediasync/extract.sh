6d6c00822abcf02f403d219fca6209eab0f53dac
Gabriel Diego Teixeira <gabrieldiegoteixeira@gmail.com>
2019-08-02 14:27:41 -0700

    Added support for a different list format.

--- mediasync/extract.sh
+++ mediasync/extract.sh
@@ -2,16 +2,23 @@
 
 #This script extracts musics from a long song with all songs (like a Youtube
 # Video) and a list in the format:
+# 00:00 Lua branca
+# 02:32 A dama de ouros
+# Commented out is the support for the following format:
 # 01）风小筝-别丢下我不管 Fēng Xiǎozhēng-Don't leave me alone (00:00)
 # 02）刘增瞳-我是真的爱过你 Liú Zēngtóng-I really loved you (04:33)
 
 #TODO: Adapt to other list formats
 
-#Usage: ./extract.sh list.txt input.mp3
+#Usage: ./extract.sh list.txt input.mp3 album genre
 
 list=$1
 input=$2
-prepend_track_number_title=1
+album=$3
+genre=$4
+artist=$5
+
+prepend_track_number_title=0
 
 previous_n_seconds=0
 
@@ -19,8 +26,10 @@ counter=0
 
 while IFS= read -r line
 do
-  stime=`echo $line | cut -d "(" -f2 | cut -d ")" -f1`
+#  stime=`echo $line | cut -d "(" -f2 | cut -d ")" -f1`
+  stime=`echo $line | cut -c -5`
 
+  n_comma=`echo "$stime" | sed 's/[^:]//g' | awk '{ print length }'`
   n_comma=`echo "$stime" | sed 's/[^:]//g' | awk '{ print length }'`
 
   if [ $n_comma == 1 ]
@@ -38,8 +47,10 @@ do
     list_duration[$counter-1]=$total_time
   fi
 
-  artist_name=`echo "$line" | cut -c 6- |  cut -d "-" -f1`
-  song_name=`echo "$line" | cut -c 6- |  cut -d "-" -f2 | cut -d " " -f1`
+#  artist_name=`echo "$line" | cut -c 6- |  cut -d "-" -f1`
+#  song_name=`echo "$line" | cut -c 6- |  cut -d "-" -f2 | cut -d " " -f1`
+  artist_name=$artist
+  song_name=`echo "$line" | cut -c 7- |  cut -d "-" -f1`
 
   list_artist[$counter]=$artist_name
   list_song[$counter]=$song_name
@@ -72,6 +83,6 @@ do
     title="$zi - ${list_song[$i]}"
   fi
 
-  echo ffmpeg -y -i \"$input\" -metadata title=\"$title\" -metadata artist=\"${list_artist[$i]}\" -metadata track=$zi -ss ${list_time[$i]} -t ${list_duration[$i]} -c copy \"$output_filename\"
+  echo ffmpeg -y -i \"$input\" -metadata title=\"$title\" -metadata artist=\"${list_artist[$i]}\" -metadata album=\"$album\" -metadata genre=\"$genre\" -metadata track=$zi -ss ${list_time[$i]} -t ${list_duration[$i]} -c copy \"$output_filename\"
 done
 
